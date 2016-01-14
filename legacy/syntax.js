9223dcb5a361019823ffa429046e3f942946e316
othree <othree@gmail.com>
2016-01-14 12:01:24 +0800

    Add legacy test file

---
+++ legacy/syntax.js
@@ -0,0 +1,290 @@
+import {
+  Foo,
+  Bar as z,
+} from 'xyz'
+
+import * as xd from './../../bar' // this comment is not highlighted (matched as jsImportContainer)
+import bar from 'xyz'
+// this comment is also not highlighted 
+import xyz from 'foo'
+
+window.xd
+document.
+Promise.all
+xx.readyState
+Backbone.History
+History.pushState.isNew
+
+const Animal = 'Giraffe'
+
+// Quoted Case + Block is OK
+
+switch (Animal) {
+  case 'Animal': {
+    const meat = 'what'
+    break
+  }
+}
+
+// No Quoted Case + No Block is OK
+
+switch (Animal) {
+  case Animal:
+    const meat = 'what'
+    break
+}
+
+// No Quoted Case + Block is Not OK
+
+switch (Animal) {
+  case Animal: 
+  case Animal:
+  case Animal:
+  case Animal: {
+    const meat = 'what'
+    break
+  }
+}
+
+switch (Animal) {
+  case Ani.mal: {
+    const thing = 'what'
+    break
+  }
+}
+
+switch (Animal) {
+  case Animal(): {
+    const thing = 'what'
+    break
+  }
+}
+
+switch (Animal) {
+  case (Animal): {
+    const thing = 'what'
+    break
+  }
+}
+
+var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+return re.test(email);
+
+    let myFunctionWithDefaults = function(someNormalArgument = 1, 
+                                          argumentWithDefaultValue = 'default') {
+    }
+
+a = 
+  ()
+=> {
+  return del([
+    `${dirs.css.dist}/**/*`
+  ]);
+  console.log();
+}
+
+class Hello extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  hello() {
+    return "hello, " + this.props.name;
+  }
+
+  render() {
+    ::hello()
+  }
+}
+
+let a = 1;
+if (a) {
+  console.log(a);
+} else {
+  a = 3;
+}
+if (!a) a = 4; else a = 6;
+
+[1, 2].map((q) => Math)
+var o = {
+  a: [1, 2].map((q) => Math)
+  b: baz(42).and(fuzz),
+  c: baz(42)
+    .and(fuzz),
+}
+
+var f = () => new Z();
+f = new Z();
+
+var z = (xd) => (dx) => (xxd) => {
+  if (user) { res.json(); } else {}
+};
+
+switch (z) {
+
+  case 'a': {
+    let a = 1;
+
+  }
+
+  default:
+    throw new Error();
+
+}
+for (let a in arr) {
+}
+
+const xd;
+
+var z = function (xd) {
+  for (let a in arr) {
+  }
+  return (zz);
+};
+
+var z = (xd) => {}
+
+// this one is highlighted correctly
+
+if (option.method === 'POST') {
+  let body = ''
+  Object.keys(params).forEach((key) => {
+    let value = params[key]
+    if (typeof value !== 'string') {
+      value = JSON.stringify(value)
+    }
+    if (value !== undefined) {
+      for (let a in arr) {
+      }
+      body += `${xd}xxxx ${ encodeURIComponent(key) + 1 ? 'zz' : true }=${ encodeURIComponent(value) }&`
+    }
+  })
+  options.body = body
+} else {
+  // ... more code
+}
+
+
+if (!/foobar/.test(string)) {
+}
+
+var obj = {
+  xd: xd('path/*.ext'),
+  resolve: /* xx */ function (zz = yy()) {
+  },
+  zzz: 123
+};
+
+if (parseInt(x)) {
+  return parseInt(y), parseInt(y);
+}
+
+parseInt(z);
+
+var og = {
+  *auth() {
+  }
+};
+
+export class Counter extends React.Component {
+  static propTypes = { initialCount: React.PropTypes.number };
+  static defaultProps = { initialCount: 0 };
+  state = { count: this.props.initialCount };
+  tick(x = z()) {
+    this.setState({ count: this.state.count + 1 });
+    if ($$xx) {
+      $$aa.remove(`${this.$$prefix}.keys`);
+    }
+    xd;
+    if (user) { res.json(); } else {}
+  }
+  render() {
+    // return (
+      // <div onClick={this.tick.bind(this)}>
+        // Clicks: {this.state.count}
+      // </div>
+    // );
+  }
+}
+
+
+if (info.Config.Cmd) {
+  info.Config.Cmd.forEach(function (e, i, a) {
+    if (/[; &\\]/.test(e)) {
+      a[i];
+    }
+  });
+}
+
+function(foo /*,bar*/) { }
+function(foo, bar) { }
+
+document.location.href;
+
+Object.preventExtensions()
+
+
+var xx = new Promise(function (res) {
+  res(new Response());
+});
+
+
+var n = document.getElementById();
+
+b.onclick = function (event) {
+  event
+}
+
+n.addEventListener('click', function (event) {
+  event.
+}, false)
+
+
+
+xx.then(function (res) {
+  res.
+  return '';
+}).then(function (t) {
+  t.l
+})
+
+var x = fetch('http://xd');
+x.then(function (res) {
+  res.arrayBuffer().then(function (b) {
+    b.b
+  })
+});
+
+var reg = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@a-zA-Z0-9?(?:.a-zA-Z0-9?)$/;
+var exp = new aa({
+ aa: {
+   xd: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+ },
+ dx: 1
+});
+
+let a = 1;
+const b = 2;
+
+class Foo extends A {
+  mde() {
+    super.mde();
+  }
+};
+function Foo () {
+  let a = 1;
+  const b = 2;
+  return class Foo {
+    m() {
+      super.m();
+    }
+  };
+}
+
+export default react => (a, b, c
+                        ) => {
+  
+
+}
+
+
