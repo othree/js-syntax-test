56f44fb3d1e81fd102e8d35654b9d8fb8373d2ab
othree <othree@gmail.com>
2016-01-14 18:39:08 +0800

    Catch if is Firefox only

--- syntax/try-catch.js
+++ syntax/try-catch.js
@@ -7,31 +7,6 @@ catch (e) {
 }
 
 try {
-// 可以拋出三個例外的函數
-   getCustInfo("Lee", 1234, "lee@netscape.com")
-}
-
-catch (e if e == "InvalidNameException") {
-// 針對無效的名稱呼叫處理器
-   bad_name_handler(e)
-}
-
-catch (e if e == "InvalidIdException") {
-// 針對無效的 ID 呼叫處理器
-   bad_id_handler(e)
-}
-
-catch (e if e == "InvalidEmailException") {
-// 針對無效的電子郵件位址呼叫處理器
-   bad_email_handler(e)
-}
-
-catch (e){
-// 不知到該做什麼，就記在日誌裡
-   logError(e)
-}
-
-try {
    writeMyFile(theData); // 這裡有可能拋出錯誤
 }catch(e){
    handleError(e); // 如果我們得到錯誤，就處理他
