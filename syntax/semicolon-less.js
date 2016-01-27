b46d0867b52381bda317d8f845a6a7d83cb2ee3d
othree <othree@gmail.com>
2016-01-27 12:14:12 +0800

    Add one more case

---
+++ syntax/semicolon-less.js
@@ -0,0 +1,42 @@
+
+Tempplate.testing.helpers({
+  helper1 () {
+  
+    let test1 = hello.world()
+    // xd
+  
+  }
+})
+
+
+console.log('hello')
+console.log('hello')
+console.log('hello')
+var a = 1
+var b = 2
+var c = 3
+
+function foo(a, b) {
+  console.log('hello')
+  console.log('hello')
+  console.log('hello')
+  var a = 1
+  var b=2
+  var c=3
+}
+
+function bar() {
+  var a = 1
+  var b = 2
+  var c = 3
+  console.log('hello')
+  console.log('hello')
+  console.log('hello')
+}
+
+console.log('hello')
+console.log('hello')
+console.log('hello')
+var a = 1
+var b = 2
+var c = 3
