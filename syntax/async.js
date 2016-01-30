9d088a5eb8aa51e5ec8710ad90835ef6a907a0ae
othree <othree@gmail.com>
2016-01-30 21:10:16 +0800

    Add async

---
+++ syntax/async.js
@@ -0,0 +1,18 @@
+const xs = [];
+
+describe('Foo', () => {
+
+  const ys = [];
+
+  context('GET /foo/:fooId', () => {
+
+    const zs = [];
+
+    it('should', async () => {
+      const ws =[];
+    });
+  
+  
+  });
+
+});
