1d54b70e3fde23c74aba84ac8b99b48f932c6507
othree <othree@gmail.com>
2016-02-17 14:20:00 +0800

    Add promise test case

---
+++ syntax/promise.js
@@ -0,0 +1,12 @@
+return {
+  x: Promise.all(() => {
+    return new Class({})
+      .fetch({foo: bar})
+      .then(res => {
+        let x
+      })
+      .then((res) => {
+        let x
+      })
+  }
+}
