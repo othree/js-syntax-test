7a435c46b8415aea003b8ba8d01581ab4bb2ba38
othree <othree@gmail.com>
2016-02-22 16:02:52 +0800

    Add template string sample

---
+++ syntax/template-string.js
@@ -0,0 +1,10 @@
+
+var str1 = `abcde`;
+
+var str2 = `abcde${str1}abcde`;
+
+var str3 = `abcde${(function () {}())}abcde`;
+
+var str4 = `abcde${({prop1: 1, prop2: 'p'})['prop1']}abcde`;
+
+var str5 = `abcde${({prop1: 1, prop2: `edcba${1}ebcda`})['prop1']}abcde`;
