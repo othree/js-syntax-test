bbfe898112582201f1a80d46ab58b0eb5ae61615
othree <othree@gmail.com>
2016-02-22 16:13:00 +0800

    Multi line template string

--- syntax/template-string.js
+++ syntax/template-string.js
@@ -8,3 +8,8 @@ var str3 = `abcde${(function () {}())}abcde`;
 var str4 = `abcde${({prop1: 1, prop2: 'p'})['prop1']}abcde`;
 
 var str5 = `abcde${({prop1: 1, prop2: `edcba${1}ebcda`})['prop1']}abcde`;
+
+var str6 = `abcde${({
+  prop1: 1, 
+  prop2: `edcba${1}ebcda`
+})['prop1']}abcde`;
