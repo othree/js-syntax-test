6db6ffac94bd118b307dada71ede45b49edae4fa
othree <othree@gmail.com>
2016-03-03 11:25:20 +0800

    Update arrow function case

--- syntax/arrow.js
+++ syntax/arrow.js
@@ -1,4 +1,13 @@
+
+function xd (arg) { return arg; }
+
+let p = (arg) => {};
+
 let s = new Set(arr.filter((x) => { return x.p == 1; }));
 
-((xd) => false)
+((foo) => false)
+( (foo) => false)
+( 
+ (foo) => false
+)
 
