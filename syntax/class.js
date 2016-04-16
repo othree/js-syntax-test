25922864cbf798a6828dd13ea0318749eac7cd27
othree <othree@gmail.com>
2016-04-16 10:04:18 +0800

    Add anonymous class

---
+++ syntax/class.js
@@ -0,0 +1,27 @@
+
+class Foo {
+  constructor() {
+  }
+
+  method1 () {
+  }
+}
+
+class Bar extends foo {
+  constructor() {
+  }
+
+  method1 () {
+  }
+
+  method2 () {
+  }
+}
+
+var Foobar = class {
+  constructor () {
+  }
+
+  method1 () {
+  }
+}
