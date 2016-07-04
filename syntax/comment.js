e5e3d6fd8c021acbb2993ce5e7d127efa6d06566
othree <othree@gmail.com>
2016-07-04 19:09:13 +0800

    Add long comment

---
+++ syntax/comment.js
@@ -0,0 +1,46 @@
+/*
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis justo
+  convallis, dictum erat sed, pellentesque felis. Proin eget lacinia mi, quis
+  maximus lectus. Cras a orci nunc. Nunc in dolor accumsan, lobortis nunc eget,
+  consectetur massa. Curabitur quis nulla orci. Pellentesque mauris mi, sodales a
+  nisl eu, laoreet maximus dui. Cras pretium molestie varius. In et lorem et diam
+  lobortis euismod.
+
+  Nullam hendrerit massa a bibendum tempor. Sed dictum tortor at auctor commodo.
+  In elit sem, bibendum eget dignissim et, posuere id erat. Phasellus tempor in
+  nulla ac finibus. Nullam hendrerit in urna sed tempor. Duis sapien dolor,
+  consequat efficitur efficitur sed, scelerisque ac ante. Quisque id erat at ipsum
+  ornare aliquam in egestas dui. Nam vestibulum diam eget eleifend sollicitudin.
+  Morbi eget suscipit justo, a finibus sem. Aenean consequat lectus ut pretium
+  cursus. Ut eget eros tellus. Phasellus facilisis eleifend mollis.  Aliquam
+  viverra ipsum eu ipsum tempor, sed faucibus eros tempus. Duis pharetra ac odio
+  nec porttitor. Nam cursus luctus diam sit amet pharetra. Maecenas dapibus
+  ullamcorper tristique. Sed in porttitor mi. Suspendisse nisl metus, scelerisque
+  ac odio quis, egestas volutpat augue. Donec semper accumsan est a ultrices.
+  Donec eu auctor leo. Morbi eros massa, hendrerit vitae vulputate in, elementum
+  quis metus. Vestibulum velit quam, luctus vel purus a, viverra suscipit ligula.
+  Nullam quis molestie mauris. Duis nunc eros, pellentesque non bibendum sit
+  amet, imperdiet mattis dui. Curabitur et erat vestibulum nunc efficitur finibus
+  ut pharetra urna.
+
+  Mauris luctus sodales elit in ornare. Mauris cursus ipsum metus, et eleifend ex
+  luctus ut. Praesent finibus odio congue leo tincidunt, id tincidunt eros luctus.
+  Cras sit amet erat eu justo vehicula facilisis. Praesent vel augue sit amet
+  dui semper facilisis non vitae augue. Donec non venenatis risus. Nam auctor eros
+  et leo convallis, ac ullamcorper mi egestas. Nunc efficitur pulvinar purus, et
+  fermentum nisl fermentum non. Nam nec lorem feugiat, laoreet arcu ut, posuere
+  quam.  Nam ullamcorper hendrerit quam cursus iaculis. Phasellus interdum augue
+  et efficitur tincidunt. Praesent consectetur leo sit amet felis consectetur,
+  vitae cursus sapien suscipit. Phasellus sit amet libero dui. Nulla faucibus nunc
+  justo, sit amet congue libero egestas id. Duis vehicula, odio vitae maximus
+  luctus, sapien justo tincidunt elit, suscipit dictum elit orci non elit. Quisque
+  eu sapien convallis, euismod lectus sit amet, auctor neque. Curabitur rutrum
+  porta nulla, commodo laoreet ante laoreet vel. Proin fringilla dolor mi, in
+  sagittis quam iaculis eget. Cum sociis natoque penatibus et magnis dis
+  parturient montes, nascetur ridiculus mus. Aenean vitae tortor nibh. Donec
+  maximus tincidunt ligula, euismod semper justo lacinia quis. Maecenas facilisis
+  pretium lacus at porttitor. 
+*/
+
+
+
