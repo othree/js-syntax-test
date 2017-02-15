ff3ddda0401faf57b459e0830fe32ab0475165c9
othree <othree@gmail.com>
2017-02-15 18:23:05 +0800

    More type hints

--- syntax/type.js
+++ syntax/type.js
@@ -5,6 +5,8 @@ let offTheEnd: number = array[100]; // No error
 
 let array2: Array<string> = ["an alternate", "syntax", "for arrays"];
 
+let p: Promise<*> = fetch('');
+
 function isNumber(x: any): x is number {
     return typeof x === "number";
 }
@@ -21,4 +23,21 @@ function padLeft(value: string, padding: string | number) {
         return padding + value;
     }
     throw new Error(`Expected string or number, got '${padding}'.`);
+}
+
+var a:string = 'a';
+
+async function pullOne (id: string): Promise<*> {
+  // <- code here seems to be still considered as a type
+  return 1;
+}
+
+class test {
+  async pullZero (id: string) {
+    return 0;
+  }
+  async pullOne (id: string): Promise<*> {
+    // <- code here seems to be still considered as a type
+    return 1;
+  }
 }
