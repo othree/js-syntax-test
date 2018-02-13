1ab38be4ab1272f2d9456eda6605b0b59f0054e0
othree <othree@gmail.com>
2018-02-13 15:25:56 +0800

    Simple type annotation

---
+++ syntax/type.ts
@@ -0,0 +1,42 @@
+
+let array: number[] = [1, 2, 3.14, 42];
+let theAnswer: number = array[3]; // 42
+let offTheEnd: number = array[100]; // No error
+
+let array2: Array<string> = ["an alternate", "syntax", "for arrays"];
+
+let p: Promise<*> = fetch('');
+
+function isNumber(x: any): x is number {
+    return typeof x === "number";
+}
+
+function isString(x: any): x is string {
+    return typeof x === "string";
+}
+
+function padLeft(value: string, padding: string | number) {
+    if (isNumber(padding)) {
+        return Array(padding + 1).join(" ") + value;
+    }
+    if (isString(padding)) {
+        return padding + value;
+    }
+    throw new Error(`Expected string or number, got '${padding}'.`);
+}
+
+var a:string = 'a';
+
+async function pullOne (id: string): Promise<*> {
+  // comment!!
+  return 1;
+}
+
+class test {
+  async pullZero (id: string) {
+    return 0;
+  }
+  async pullOne (id: string): Promise<*> {
+    return 1;
+  }
+}
